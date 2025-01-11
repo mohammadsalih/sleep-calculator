c72f8ca07ae3509ce764626941f025835792ee09
mohammadsalih <mohammadsalih.main@gmail.com>
2025-01-11 23:41:59 +0300

    design changes

--- src/layout/SleepInfo.jsx
+++ src/layout/SleepInfo.jsx
@@ -1,5 +1,11 @@
+import SleepTimeContainer from '../ui/SleepTimeContainer';
+
 function SleepInfo() {
-  return <div className='sleep-info'>hey</div>;
+  return (
+    <div className='sleep-info'>
+      <SleepTimeContainer />
+    </div>
+  );
 }
 
 export default SleepInfo;
---
+++ src/ui/SleepTimeContainer.jsx
@@ -0,0 +1,11 @@
+function SleepTimeContainer() {
+  return (
+    <div className='sleep-time-container'>
+      <h2 className='cycles'>
+        12:12 <span>AM</span>
+      </h2>
+    </div>
+  );
+}
+
+export default SleepTimeContainer;
